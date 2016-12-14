9043daf84479e6f6021b3eb6d79b2fe1bb43a1ad
brm-jbarraganj <jeisson.barragan@brm.com.co>
2016-12-14 11:27:27 -0500

    Iniciando el admin

---
+++ server/control.php
@@ -0,0 +1,47 @@
+<?php 
+	if (isset($_POST['control']) && !empty($_POST['control']) ) {
+		
+		$control = $_POST['control'];
+		$obj = new Admin();
+
+		switch ($control) {
+
+			
+			case '':
+				
+				break;
+			
+			case '':
+				
+				break;
+
+			case '':
+				
+				break;
+
+			case '':
+				
+				break;
+
+			case '':
+				
+				break;
+
+			case '':
+				
+				break;
+
+			case '':
+				
+				break;
+
+			case '':
+				
+				break;
+
+			case '':
+				
+				break;
+		}//end switch
+	}//end if 
+?>
